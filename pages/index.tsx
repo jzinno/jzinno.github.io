b6e5b17a99ff0422fe352551ee859ee0de67dfda
John Zinno <johnzinno14@gmail.com>
2023-06-01 17:48:17 -0400

    update style and content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,27 @@
-import Head from 'next/head';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Head from "next/head";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen bg-gray-100">
       <Head>
         <title>John Zinno</title>
       </Head>
 
       <Header />
-
-      <main>
-        <h1>Welcome to My Resume Website</h1>
-        <p>Here you can find my qualifications and experience.</p>
+      <main className="flex justify-center items-center h-64 bg-gray-100 w-full">
+        <div className="flex justify-center items-center h-32 max-w-4xl bg-slate-800 rounded-lg drop-shadow-lg">
+          <div>
+            <h1 className="text-3xl text-slate-400 text-center select-none">
+              Welcome to My Website
+            </h1>
+            <p className="text-xl text-slate-400 text-center select-none px-4">
+              Here you can find my qualifications, experience, projects, and
+              interests.
+            </p>
+          </div>
+        </div>
       </main>
       <Footer />
     </div>
