6e64c5989ead41eeabedebe070da83dd2a28cbb8
John Zinno <johnzinno14@gmail.com>
2023-06-01 17:47:50 -0400

    update components style and content

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,9 @@
 const Footer: React.FC = () => {
-    return (
-      <footer className="bg-gray-200 py-4 text-center fixed bottom-0 left-0 w-full">
-        <p>&copy; {new Date().getFullYear()} John Zinno</p>
-      </footer>
-    );
-  };
-  
-  export default Footer;
-  
+  return (
+    <footer className="bg-gradient-to-t from-gray-200 py-4 text-center fixed bottom-0 left-0 w-full">
+      <p>&copy; {new Date().getFullYear()} John Zinno</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,14 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-gray-800 py-4">
-      <nav className="max-w-3xl mx-auto flex justify-between">
+    <header className="bg-slate-800 py-4 drop-shadow-md opacity-90">
+      <nav className="max-w-xl mx-auto flex justify-between">
         <Link href="/" className="text-white text-xl font-bold">
           Home
         </Link>
-        <Link href="/resume" className="text-white text-xl font-bold">
-          Resume
+        <Link href="/aboutme" className="text-white text-xl font-bold">
+          About Me
         </Link>
       </nav>
     </header>
