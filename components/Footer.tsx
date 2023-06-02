95053f6fe10956ec3dcec6e271150e1c87b656e9
John Zinno <johnzinno14@gmail.com>
2023-06-02 09:14:32 -0400

    footer reformat

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gradient-to-t from-gray-200 py-4 text-center fixed bottom-0 left-0 w-full">
+    <footer className="py-4 text-center fixed bottom-0 left-0 w-full">
       <p>&copy; {new Date().getFullYear()} John Zinno</p>
     </footer>
   );
