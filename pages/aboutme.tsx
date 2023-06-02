ec16685232ada20b8256f6eafd959dbeeeabcebc
John Zinno <johnzinno14@gmail.com>
2023-06-02 09:15:15 -0400

    small changes to about me

--- pages/aboutme.tsx
+++ pages/aboutme.tsx
@@ -1,20 +1,22 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import WaveComponent from "../components/WaveComponent";
 
 const Resume: React.FC = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen bg-gray-100">
       <Head>
         <title>Resume - John Zinno</title>
       </Head>
-
       <Header />
-
       <main>
         <h1>Some content</h1>
         {/* Add your resume content here */}
       </main>
+      <div className="text-center fixed bottom-0 left-0 w-full">
+        <WaveComponent startColor="#ff5e62" endColor="#ff9966" />
+      </div>
       <Footer />
     </div>
   );
