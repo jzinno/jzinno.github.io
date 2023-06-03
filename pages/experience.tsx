61d1cecc706e54be4ee28e8acb50449a680c5a91
jzinno <johnzinno14@gmail.com>
2023-06-03 10:20:28 -0400

    add some placeholder pages

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -3,16 +3,16 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import WaveComponent from "../components/WaveComponent";
 
-const AboutMe: React.FC = () => {
+const Experience: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
       <Head>
-        <title>About Me - John Zinno</title>
+        <title>Experience - John Zinno</title>
       </Head>
       <Header />
       <main>
         <h1>Some content</h1>
-        {/* Add your AboutMe content here */}
+        {/* Add your Experience content here */}
       </main>
       <div className="text-center fixed bottom-0 left-0 w-full">
         <WaveComponent startColor="#ff5e62" endColor="#ff9966" />
@@ -22,4 +22,4 @@ const AboutMe: React.FC = () => {
   );
 };
 
-export default AboutMe;
+export default Experience;
