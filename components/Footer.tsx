a65fb42e78234f4f1cd972f1c11659eab1bbea3b
jzinno <johnzinno14@gmail.com>
2023-06-03 10:21:36 -0400

    reduce padding on text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer: React.FC = () => {
   return (
-    <footer className="py-4 text-center fixed bottom-0 left-0 w-full">
+    <footer className="py-2 text-center fixed bottom-0 left-0 w-full select-none text-black">
       <p>&copy; {new Date().getFullYear()} John Zinno</p>
     </footer>
   );
