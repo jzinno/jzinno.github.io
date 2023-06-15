bc3ca99a1204c4e6030fa32f11ae26d7cb24c46d
John Zinno <johnzinno14@gmail.com>
2023-06-15 11:30:09 -0400

    update placeholders

--- components/ThreeSlotFooter.tsx
+++ components/ThreeSlotFooter.tsx
@@ -1,18 +1,17 @@
 const Footer: React.FC = () => {
-    return (
-      <footer className="flex justify-between py-4 text-center fixed bottom-0 left-0 w-full">
-        <div>
-          <p>Content for div 1</p>
-        </div>
-        <div>
-          <p>&copy; {new Date().getFullYear()} John Zinno</p>
-        </div>
-        <div>
-          <p>Content for div 3</p>
-        </div>
-      </footer>
-    );
-  };
-  
-  export default Footer;
-  
+  return (
+    <footer className="flex justify-between py-4 text-center fixed bottom-0 left-0 w-full">
+      <div>
+        <p>not sure yet</p>
+      </div>
+      <div>
+        <p>&copy; {new Date().getFullYear()} John Zinno</p>
+      </div>
+      <div>
+        <p>Logo images here of stuff used to make</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
