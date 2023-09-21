5a722e1eed9e3aa2f6261c4aa1138f8732078a31
John Zinno <johnzinno14@gmail.com>
2023-09-21 09:58:34 -0400

    added template boilerplate

--- pages/aboutme.tsx
+++ pages/aboutme.tsx
@@ -1,3 +1,4 @@
+
 import Head from "next/head";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -11,8 +12,12 @@ const AboutMe: React.FC = () => {
       </Head>
       <Header />
       <main>
-        <h1>Some content</h1>
-        {/* Add your AboutMe content here */}
+        <section className="text-center py-12 px-4">
+          <h1 className="text-4xl mb-4">About Me</h1>
+          <p className="text-xl text-gray-700">
+            Hello! I'm John Zinno, a full-stack developer with a passion for problem-solving and creating intuitive user experiences. With a background in Computer Science and several years of experience in the tech industry, I have a strong understanding of software development principles and modern technologies.
+          </p>
+        </section>
       </main>
       <div className="text-center fixed bottom-0 left-0 w-full">
         <WaveComponent startColor="#ff5e62" endColor="#ff9966" />
