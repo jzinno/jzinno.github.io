b3069f6eeef7f59da74c5a2cd99b002e04a18647
John Zinno <johnzinno14@gmail.com>
2023-09-22 08:31:27 -0400

    update footer and format

--- pages/aboutme.tsx
+++ pages/aboutme.tsx
@@ -1,7 +1,6 @@
-
 import Head from "next/head";
 import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Footer from "../components/ThreeSlotFooter";
 import WaveComponent from "../components/WaveComponent";
 
 const AboutMe: React.FC = () => {
@@ -15,7 +14,11 @@ const AboutMe: React.FC = () => {
         <section className="text-center py-12 px-4">
           <h1 className="text-4xl mb-4">About Me</h1>
           <p className="text-xl text-gray-700">
-            Hello! I'm John Zinno, a full-stack developer with a passion for problem-solving and creating intuitive user experiences. With a background in Computer Science and several years of experience in the tech industry, I have a strong understanding of software development principles and modern technologies.
+            Hello! I'm John Zinno, a full-stack developer with a passion for
+            problem-solving and creating intuitive user experiences. With a
+            background in Computer Science and several years of experience in
+            the tech industry, I have a strong understanding of software
+            development principles and modern technologies.
           </p>
         </section>
       </main>
