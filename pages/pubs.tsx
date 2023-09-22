c89826d989cda0321f9e95493b11fab46a6f6e9e
John Zinno <johnzinno14@gmail.com>
2023-09-22 08:35:33 -0400

    update footer

--- pages/pubs.tsx
+++ pages/pubs.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Footer from "../components/ThreeSlotFooter";
 import WaveComponent from "../components/WaveComponent";
 
 const Pubs: React.FC = () => {
