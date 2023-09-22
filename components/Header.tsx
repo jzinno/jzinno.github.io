f73c42c65103c5d08ec03860b3b8a71a0dbc3a16
John Zinno <johnzinno14@gmail.com>
2023-09-22 08:29:55 -0400

    refactor header and add responsivness

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,24 +1,26 @@
 import Link from "next/link";
 
+const links = [
+  { href: "/", text: "Home" },
+  { href: "/experience", text: "Experience" },
+  { href: "/edu", text: "Education" },
+  { href: "/pubs", text: "Publications" },
+  { href: "/gallery", text: "Gallery" },
+];
+
 const Header: React.FC = () => {
   return (
-    <header className="bg-slate-800 py-4 drop-shadow-lg opacity-90">
-      <nav className="max-w-4xl mx-auto flex justify-between">
-        <Link href="/" className="text-white text-xl font-bold">
-          Home
-        </Link>
-        <Link href="/experience" className="text-white text-xl font-bold">
-          Experience
-        </Link>
-        <Link href="/edu" className="text-white text-xl font-bold">
-          Education
-        </Link>
-        <Link href="/pubs" className="text-white text-xl font-bold">
-          Publications
-        </Link>
-        <Link href="/gallery" className="text-white text-xl font-bold">
-          Gallery
-        </Link>
+    <header className="bg-slate-800 py-4 shadow-lg opacity-90">
+      <nav className="max-w-max mx-auto flex justify-between items-center divide-dotted divide-x-2 ">
+        {links.map((link, index) => (
+          <Link
+            key={index}
+            href={link.href}
+            className="px-4 hover:px-6 text-white text-xl font-bold hover:text-gray-300 hover:scale-105 cursor-pointer select-none transition-all duration-200 ease-in-out"
+          >
+            {link.text}
+          </Link>
+        ))}
       </nav>
     </header>
   );
