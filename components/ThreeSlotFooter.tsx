366b48a7ba2a923c68d41613373bca095c6509e9
John Zinno <johnzinno14@gmail.com>
2023-09-22 08:30:41 -0400

    add github link icon to footer

--- components/ThreeSlotFooter.tsx
+++ components/ThreeSlotFooter.tsx
@@ -1,14 +1,25 @@
 const Footer: React.FC = () => {
   return (
-    <footer className="flex justify-between py-4 text-center fixed bottom-0 left-0 w-full">
+    <footer className="flex justify-between py-4 text-center fixed bottom-0 left-0 w-full p-4">
       <div>
-        <p>not sure yet</p>
+        <p></p>
       </div>
       <div>
         <p>&copy; {new Date().getFullYear()} John Zinno</p>
       </div>
-      <div>
-        <p>Logo images here of stuff used to make</p>
+      <div className="item-center opacity-70 hover:shadow-2xl transition duration-500 ease-in-out transform hover:opacity-100 hover:-translate-y-1 hover:scale-110">
+        <a
+          href="https://github.com/jzinno"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/github-mark/github-mark.svg"
+            alt="Github logo"
+            width="24"
+            height="24"
+          />
+        </a>
       </div>
     </footer>
   );
