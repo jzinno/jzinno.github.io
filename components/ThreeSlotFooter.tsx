522ab99ba159f362fec15b8e05fab61c14807bf3
jzinno <johnzinno14@gmail.com>
2023-09-22 10:57:30 -0400

    migrate icons to react-icons

--- components/ThreeSlotFooter.tsx
+++ components/ThreeSlotFooter.tsx
@@ -1,3 +1,6 @@
+import { FiMail } from 'react-icons/fi';
+import { FaGithub } from 'react-icons/fa';
+
 const Footer: React.FC = () => {
   return (
     <footer className="flex justify-between py-4 text-center fixed bottom-0 left-0 w-full p-4">
@@ -7,20 +10,7 @@ const Footer: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
-            />
-          </svg>
+          <FiMail className="w-6 h-6" />
         </a>
       </div>
       <div className="text-sm opacity-70">
@@ -32,12 +22,7 @@ const Footer: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img
-            src="/github-mark/github-mark.svg"
-            alt="Github logo"
-            width="24"
-            height="24"
-          />
+          <FaGithub className="w-6 h-6" />
         </a>
       </div>
     </footer>
