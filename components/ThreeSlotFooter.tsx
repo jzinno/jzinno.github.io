0c301d096de5f9d024351ceeca428d49bb49e7d1
jzinno <johnzinno14@gmail.com>
2023-09-22 15:16:50 -0400

    tweak response time

--- components/ThreeSlotFooter.tsx
+++ components/ThreeSlotFooter.tsx
@@ -4,7 +4,7 @@ import { FaGithub } from 'react-icons/fa';
 const Footer: React.FC = () => {
   return (
     <footer className="flex justify-between py-4 text-center fixed bottom-0 left-0 w-full p-4">
-      <div className="item-center opacity-70 hover:shadow-2xl transition duration-500 ease-in-out transform hover:opacity-100 hover:-translate-y-1 hover:scale-110">
+      <div className="item-center opacity-70 hover:shadow-2xl transition duration-200 ease-in-out transform hover:opacity-100 hover:-translate-y-1 hover:scale-110">
         <a
           href="mailto:johnzinno14@gmail.com"
           target="_blank"
@@ -16,7 +16,7 @@ const Footer: React.FC = () => {
       <div className="text-sm opacity-70">
         <p>&copy; {new Date().getFullYear()} John Zinno</p>
       </div>
-      <div className="item-center opacity-70 hover:shadow-2xl transition duration-500 ease-in-out transform hover:opacity-100 hover:-translate-y-1 hover:scale-110">
+      <div className="item-center opacity-70 hover:shadow-2xl transition duration-200 ease-in-out transform hover:opacity-100 hover:-translate-y-1 hover:scale-110">
         <a
           href="https://github.com/jzinno"
           target="_blank"
