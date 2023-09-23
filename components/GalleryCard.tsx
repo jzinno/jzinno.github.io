0fbfffc3af2a507f06b99e0c75bf2db77bad869d
John Zinno <johnzinno14@gmail.com>
2023-09-23 00:42:02 -0400

    add gallery page

---
+++ components/GalleryCard.tsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import { FiX } from "react-icons/fi";
+
+interface GalleryCardProps {
+  featureImage: string;
+  title: string;
+  about: string;
+  blog: string;
+}
+
+const GalleryCard: React.FC<GalleryCardProps> = ({
+  featureImage,
+  title,
+  about,
+  blog,
+}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleCardClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <div
+        className="bg-white rounded-lg shadow-md overflow-hidden cursor-pointer select-none text-center hover:shadow-lg hover:scale-105 transition-all duration-200 ease-in-out"
+        onClick={handleCardClick}
+      >
+        <div className="relative h-64">
+          <div className="mx-2 my-2 relative">
+            <div className="relative h-64">
+              <Image
+                src={featureImage}
+                alt={title}
+                className="w-full h-full object-contain"
+                width={600}
+                height={600}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="p-4">
+          <h2 className="text-lg text-slate-800 font-bold mb-2">{title}</h2>
+        </div>
+      </div>
+      {isModalOpen && (
+        <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 backdrop-filter backdrop-blur-sm flex justify-center items-center z-50">
+          <div className="bg-white rounded-lg shadow-md overflow-hidden w-11/12 relative">
+            <div className="p-4 overflow-y-auto">
+              <div className="flex flex-col md:flex-row md:items-center">
+                <div className="md:w-3/5 md:mr-4 mb-2">
+                  <div className="m-2">
+                    <Image
+                      src={featureImage}
+                      alt={title}
+                      className="w-full h-full object-contain"
+                      width={600}
+                      height={600}
+                    />
+                  </div>
+                </div>
+                <div className="flex-1">
+                  <div className="flex justify-end">
+                    <button
+                      className="text-gray-700 text-xl absolute top-2 right-4"
+                      onClick={handleModalClose}
+                    >
+                      <FiX className="hover:scale-110 transition-all duration-200 ease-in-out" />
+                    </button>
+                  </div>
+                  <div className="text-center">
+                    {title && <h2 className="text-m font-bold m-2">{title}</h2>}
+                    <div className="max-h-[40vh] overflow-y-auto pb-0">
+                      <p className="text-gray-700 mb-2 ">{about}</p>
+                    </div>
+                    <p className="text-blue-500 mb-2">
+                      <a href={blog}>{blog}</a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default GalleryCard;
--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,19 +1,55 @@
 import Head from "next/head";
-import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 import Footer from "../components/ThreeSlotFooter";
 import WaveComponent from "../components/WaveComponent";
+import GalleryCard from "../components/GalleryCard";
 
 const Gallery: React.FC = () => {
+  const galleryItems = [
+    {
+      featureImage: "/TP53_diffmap.png",
+      title: "Phenotype - Genotype Linkage",
+      about: "This is the first image in the gallery",
+      blog: "",
+    },
+    {
+      featureImage: "/ball_abc.png",
+      title:
+        "Single cell  whole genome sequencing Approximate Bayesian Computation",
+      about: "This is the first image in the gallery",
+      blog: "",
+    },
+    {
+      featureImage: "/omicron.png",
+      title: "Evolution of SARS-CoV-2",
+      about:
+        "High transmissibility is a hallmark of the Omicron variant of SARS-CoV-2. Understanding the molecular determinants of Omicron’s transmissibility will impact development of intervention strategies. Here we map the electrostatic potential surface of the Spike protein to show that major SARS-CoV-2 variants have accumulated positive charges in solvent-exposed regions of the Spike protein, especially its ACE2-binding interface. Significantly, the Omicron Spike-ACE2 complex has complementary electrostatic surfaces. In contrast, interfaces between Omicron and neutralizing antibodies tend to have similar positively charged surfaces. Structural modeling demonstrates that the electrostatic property of Omicron’s Spike receptor binding domain (S RBD) plays a role in enhancing ACE2 recognition and destabilizing Spike-antibody complexes. Specifically, the Omicron S RBD has favorable electrostatic interaction energy with ACE2 that is 3-5 times greater than the Delta variant over a range of 20 Å, implying efficient recognition of host receptors. Computed binding affinities of six representative S RBD-antibody complexes show that Omicron can escape most antibodies targeting the ACE2-binding region of S RBD. Interestingly, a straightforward assessment of the electrostatic surfaces of 18 neutralizing antibodies correctly predicted the Omicron escape status of 80% of cases. Collectively, our structural analysis implies that Omicron S RBD interaction interfaces have been optimized to simultaneously promote access to human ACE2 receptors and evade antibodies. These findings suggest that electrostatic interactions are a major contributing factor for increased Omicron transmissibility relative to other variants",
+      blog: "",
+    },
+    {
+      featureImage: "/TP53_diffmap.png",
+      title: "Image 1",
+      about: "This is the first image in the gallery",
+      blog: "https://example.com/blog/image1",
+    },
+  ];
+
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
       <Head>
         <title>Gallery - John Zinno</title>
       </Head>
-      <Header />
-      <main>
-        <h1>Some content</h1>
-        {/* Add your Gallery content here */}
+      <main className="flex-1">
+        <h1 className="text-2xl font-bold select-none my-4 w-1/2 text-center mx-auto border-gray-300">
+          Gallery
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+          {galleryItems.map((item, index) => (
+            <GalleryCard key={index} {...item} />
+          ))}
+        </div>
       </main>
+      <Sidebar />
       <div className="text-center fixed bottom-0 left-0 w-full">
         <WaveComponent startColor="#ff5e62" endColor="#ff9966" />
       </div>
