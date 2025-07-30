d7a88eed377f5aea7708259ec87538cd5ad6a203
jzinno <62895131+jzinno@users.noreply.github.com>
2025-07-30 14:44:05 +0000

    Deploying to gh-pages from @ jzinno/jzinno.github.io@0e2a82dea7e853e19a0f834a179966856642ee12 🚀

--- dots.sh
+++ dots.sh
@@ -8,11 +8,13 @@ fi
 
 bash <(curl -s https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch)
 
-$SUDO apt update -y && $SUDO apt upgrade -y && $SUDO apt install rsync zsh bat -y
+$SUDO apt update -y && $SUDO apt upgrade -y && $SUDO apt install git rsync zsh fastfetch -y
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-$SUDO rm -rf /opt/nvim
-$SUDO tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+mkdir -p $HOME/.local
+rm -rf $HOME/.local/nvim-linux-x86_64
+tar -C $HOME/.local -xzf nvim-linux-x86_64.tar.gz
+rm nvim-linux-x86_64.tar.gz
 
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
@@ -22,23 +24,40 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/*; do
   fi
 done
 
-echo "export PATH=\"$PATH:/opt/nvim-linux-x86_64/bin\"" >> ~/.zshrc
-
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo "source ~/powerlevel10k/powerlevel10k.zsh-theme" >> ~/.zshrc
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
-
-git clone https://github.com/supercrabtree/k.git $HOME/k
-echo "source ~/k/k.sh" >> ~/.zshrc
-echo "alias k='k -h'" >> ~/.zshrc
-
+mkdir -p $HOME/.zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.zsh/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
+git clone https://github.com/supercrabtree/k.git $HOME/.zsh/k
 git clone https://github.com/jzinno/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
-echo "alias vi='nvim'" >> $HOME/.zshrc
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
-echo "source $HOME/.local/bin/env" >> ~/.zshrc
-echo "alias bat='batcat --paging=never'" >> ~/.zshrc
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
-sed -i '1i bash <(curl -s https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch)' ~/.zshrc
+source $HOME/.cargo/env
+
+curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+
+cargo binstall --no-confirm ripgrep bat fd-find du-dust hyperfine
+
+# Configure .zshrc
+echo "# PATH exports" >> $HOME/.zshrc
+echo "export PATH=\"$HOME/.local/nvim-linux-x86_64/bin:\$PATH\"" >> $HOME/.zshrc
+echo "" >> $HOME/.zshrc
+
+echo "# Tool sources" >> $HOME/.zshrc
+echo "source $HOME/.zsh/powerlevel10k/powerlevel10k.zsh-theme" >> $HOME/.zshrc
+echo "source $HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> $HOME/.zshrc
+echo "source $HOME/.zsh/k/k.sh" >> $HOME/.zshrc
+echo "source $HOME/.local/bin/env" >> $HOME/.zshrc
+echo "source $HOME/.cargo/env" >> $HOME/.zshrc
+echo "eval \"\$(zoxide init zsh)\"" >> $HOME/.zshrc
+echo "" >> $HOME/.zshrc
+
+echo "# Aliases" >> $HOME/.zshrc
+echo "alias vi='nvim'" >> $HOME/.zshrc
+echo "alias bat='bat --paging=never'" >> $HOME/.zshrc
+echo "alias k='k -h'" >> $HOME/.zshrc
+echo "alias rm='rm -f'" >> $HOME/.zshrc
+
+sed -i '1i command -v fastfetch >/dev/null && fastfetch' $HOME/.zshrc
