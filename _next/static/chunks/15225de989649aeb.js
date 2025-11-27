e92cd14277620047d8432dd572ea64106a13ee08
jzinno <62895131+jzinno@users.noreply.github.com>
2025-11-27 16:49:22 +0000

    Deploying to gh-pages from @ jzinno/jzinno.github.io@6f8906b5befaf49e19840479d7929ad5de3b591b 🚀

--- _next/static/chunks/15225de989649aeb.js
+++ _next/static/chunks/15225de989649aeb.js
@@ -1,6 +1,6 @@
 __turbopack_load_page_chunks__("/_app", [
   "static/chunks/8b581be287bc76c8.js",
   "static/chunks/a62efb0a3401031d.js",
-  "static/chunks/ead7a1113200d7b4.css",
-  "static/chunks/turbopack-39e5554aba22757a.js"
+  "static/chunks/dc282c07275fd9bf.css",
+  "static/chunks/turbopack-5706076b742c1428.js"
 ])
